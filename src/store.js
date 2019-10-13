f486c34d54954b6f4d8384be8847c2546d36efc2
Daniel Lind <contact@daniellind.net>
2019-10-13 23:39:57 +0200

    Refactor store.js to split the state/getters/mutations/actions into separate exports

--- src/store.js
+++ src/store.js
@@ -3,92 +3,100 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  strict: process.env.NODE_ENV !== "production",
-  state: {
-    content: "",
-    error: "",
-    message: "",
-    contentIsJson: false,
-    contentIsJsonValue: false
+export const state = {
+  content: "",
+  error: "",
+  message: "",
+  contentIsJson: false,
+  contentIsJsonValue: false
+};
+
+export const getters = {
+  contentHasErrors(state) {
+    return state.error !== "";
   },
-  getters: {
-    contentHasErrors(state) {
-      return state.error !== "";
-    },
-    statusMessage(state, getters) {
-      if (state.message !== "") {
-        return state.message;
-      } else if (getters.contentHasErrors) {
-        return state.error;
-      } else {
-        if (state.content === "") {
-          return "Content is empty";
-        } else if (state.contentIsJsonValue) {
-          return "Content is a JSON value";
-        } else if (state.contentIsJson) {
-          return "Content is a JSON object/array";
-        }
+  statusMessage(state, getters) {
+    if (state.message !== "") {
+      return state.message;
+    } else if (getters.contentHasErrors) {
+      return state.error;
+    } else {
+      if (state.content === "") {
+        return "Content is empty";
+      } else if (state.contentIsJsonValue) {
+        return "Content is a JSON value";
+      } else if (state.contentIsJson) {
+        return "Content is a JSON object/array";
       }
-    },
-    canConvert(state, getters) {
-      return (
-        !getters.contentHasErrors &&
-        state.contentIsJson &&
-        !state.contentIsJsonValue
-      );
     }
   },
-  mutations: {
-    setContent(state, content) {
-      state.content = content;
-    },
-    setJsonFlag(state, flag) {
-      state.contentIsJson = flag;
-    },
-    setJsonValueFlag(state, flag) {
-      state.contentIsJsonValue = flag;
-    },
-    setError(state, error) {
-      state.error = error;
-    },
-    setMessage(state, message) {
-      state.message = message;
-    }
+  canConvert(state, getters) {
+    return (
+      !getters.contentHasErrors &&
+      state.contentIsJson &&
+      !state.contentIsJsonValue
+    );
+  }
+};
+
+export const mutations = {
+  setContent(state, content) {
+    state.content = content;
   },
-  actions: {
-    updateContent({ commit }, content) {
-      commit("setContent", content);
-      commit("setMessage", "");
-      if (content === "") {
-        commit("setError", "");
-        commit("setJsonFlag", false);
-        commit("setJsonValueFlag", false);
-        return;
-      }
-      try {
-        let decoded = JSON.parse(content);
-        commit("setJsonFlag", true);
-        commit("setError", "");
-        if (decoded instanceof Array || decoded instanceof Object) {
-          commit("setJsonValueFlag", false);
-        } else {
-          commit("setJsonValueFlag", true);
-        }
-      } catch (error) {
-        commit("setError", error.message);
-        commit("setJsonFlag", false);
-        commit("setJsonValueFlag", false);
-      }
-    },
-    clearContent({ dispatch }) {
-      dispatch("updateContent", "");
-    },
-    resetContent({ commit }, data) {
-      commit("setContent", data.content);
-      commit("setMessage", data.message);
+  setJsonFlag(state, flag) {
+    state.contentIsJson = flag;
+  },
+  setJsonValueFlag(state, flag) {
+    state.contentIsJsonValue = flag;
+  },
+  setError(state, error) {
+    state.error = error;
+  },
+  setMessage(state, message) {
+    state.message = message;
+  }
+};
+
+export const actions = {
+  updateContent({ commit }, content) {
+    commit("setContent", content);
+    commit("setMessage", "");
+    if (content === "") {
       commit("setError", "");
       commit("setJsonFlag", false);
+      commit("setJsonValueFlag", false);
+      return;
     }
+    try {
+      let decoded = JSON.parse(content);
+      commit("setJsonFlag", true);
+      commit("setError", "");
+      if (decoded instanceof Array || decoded instanceof Object) {
+        commit("setJsonValueFlag", false);
+      } else {
+        commit("setJsonValueFlag", true);
+      }
+    } catch (error) {
+      commit("setError", error.message);
+      commit("setJsonFlag", false);
+      commit("setJsonValueFlag", false);
+    }
+  },
+  clearContent({ dispatch }) {
+    dispatch("updateContent", "");
+  },
+  resetContent({ commit }, data) {
+    commit("setContent", data.content);
+    commit("setMessage", data.message);
+    commit("setError", "");
+    commit("setJsonFlag", false);
   }
+};
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== "production",
+  state,
+  getters,
+  mutations,
+  actions
 });
