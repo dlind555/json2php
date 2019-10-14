f5e263fc3cf8d67df7f98d0626324ecabc79e266
Daniel Lind <contact@daniellind.net>
2019-10-14 13:14:27 +0200

    Install purgecss and use it to optimize the css for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,23 @@
 /* eslint-disable */
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
   "plugins": [
     require('tailwindcss')('tailwind.js'),
     require('autoprefixer')(),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
-};
+}
